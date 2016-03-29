800ad06bba88c2c441ffc95cad7b7c4258e0bfee
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2016-03-29 10:10:07 +0100

    Added atelier routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var hbs = require('express-hbs');
 
 var routes = require('./routes/index');
 var tiles = require('./routes/tiles');
+var atelier = require('./routes/atelier');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/tiles', tiles);
+app.use('/atelier', atelier);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
