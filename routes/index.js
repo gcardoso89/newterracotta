901361331fc0a71041e5636effced72816a6e975
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2017-01-29 16:50:35 +0000

    Added button on homepage and portuguese tiles URL

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'New Terracotta - Portuguese Handmade' });
 });
 
+router.get('/portuguese-tiles', function(req, res, next) {
+  res.render('atelier/portuguese_tiles');
+});
+
 module.exports = router;
