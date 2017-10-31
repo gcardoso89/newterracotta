80822e27c5a20c216f036327a475554f2f9b4ff8
Gonçalo Cardoso <goncalocardoso@MacBook-Pro-de-Goncalo.local>
2017-10-31 10:28:23 +0000

    Added changes for Heroku

--- server.js
+++ server.js
@@ -2,47 +2,44 @@
  * Module dependencies.
  */
 
-var app = require('./app');
-var debug = require('debug')('newterracotta:server');
-var http = require('http');
+var app = require( './app' );
+var debug = require( 'debug' )( 'newterracotta:server' );
+var http = require( 'http' );
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.OPENSHIFT_NODEJS_PORT || '3000');
-app.set('port', port);
-
-var ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-
+var port = normalizePort( process.env.PORT || '3000' );
+app.set( 'port', port );
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+var server = http.createServer( app );
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port, ip);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen( port );
+server.on( 'error', onError );
+server.on( 'listening', onListening );
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-	var port = parseInt(val, 10);
+function normalizePort( val ) {
+	var port = parseInt( val, 10 );
 
-	if (isNaN(port)) {
+	if ( isNaN( port ) ) {
 		// named pipe
 		return val;
 	}
 
-	if (port >= 0) {
+	if ( port >= 0 ) {
 		// port number
 		return port;
 	}
@@ -54,8 +51,8 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
-	if (error.syscall !== 'listen') {
+function onError( error ) {
+	if ( error.syscall !== 'listen' ) {
 		throw error;
 	}
 
@@ -64,14 +61,14 @@ function onError(error) {
 		: 'Port ' + port;
 
 	// handle specific listen errors with friendly messages
-	switch (error.code) {
+	switch ( error.code ) {
 		case 'EACCES':
-			console.error(bind + ' requires elevated privileges');
-			process.exit(1);
+			console.error( bind + ' requires elevated privileges' );
+			process.exit( 1 );
 			break;
 		case 'EADDRINUSE':
-			console.error(bind + ' is already in use');
-			process.exit(1);
+			console.error( bind + ' is already in use' );
+			process.exit( 1 );
 			break;
 		default:
 			throw error;
@@ -87,5 +84,5 @@ function onListening() {
 	var bind = typeof addr === 'string'
 		? 'pipe ' + addr
 		: 'port ' + addr.port;
-	debug('Listening on ' + bind);
+	debug( 'Listening on ' + bind );
 }
